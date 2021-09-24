262576faea4c618f67ac44ec259386394e2c1a5b
MikeDudolevitch <mikedudolevitch@gmail.com>
2021-09-24 14:55:24 -0400

    fix navbar redundancy issue with imported component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import {
   Route 
 } from "react-router-dom"
 import Home from './components/Home';
+import Navigation from './components/Navigation'
 
 function App() {
   return (
     <Router>
       <div>
+        <Navigation />
         <Switch>
           <Route exact path= "/" component={Home}/>
         </Switch>
---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default class Navigation extends React.Component {
+
+    render () {
+        return (
+        <nav>
+            here is a navbar
+        </nav>
+        )
+    }
+
+}
